6de0e75eeaa2d6028f0581d7d5e752694ccff252
subinprk <subpark@student.42berlin.de>
2024-05-14 14:55:56 +0200

    a bit more error handling on ex01

---
+++ ex02/Bureaucrat.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 16:56:00 by subpark           #+#    #+#             */
+/*   Updated: 2024/05/14 14:30:53 by subpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+#include "Form.hpp"
+
+class Bureaucrat
+{
+	public:
+		Bureaucrat(); //curious about const string initialization
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(Bureaucrat &copy);
+		Bureaucrat &operator=(Bureaucrat &copy);
+		~Bureaucrat() = default;
+		std::string	getName();
+		int		getGrade();
+		void	putGrade(int grade);
+		void	gradeUp();
+		void	gradeDown();
+		void	signForm(Form &form);
+		class	GradeTooHighExeption : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		class	GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+	private:
+		const std::string	_name;
+		int					_grade;
+};
+
+std::ostream& operator<<(std::ostream &os, Bureaucrat &bureau);
+
+#endif
---
+++ ex02/Form.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/12 17:15:03 by subpark           #+#    #+#             */
+/*   Updated: 2024/05/14 14:40:55 by subpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+# include <string>
+# include <iostream>
+
+class Bureaucrat;
+class Form
+{
+	public:
+		Form(std::string name, int signGrage, int exeGrade);
+		Form(const Form &copy);
+		Form &operator = (const Form &copy);
+		~Form() = default;
+		std::string	getName();
+		int		getSignG();
+		int		getExeG();
+		bool	getBool();
+		void	beSigned(Bureaucrat &b);
+		class GradeTooHighException: public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		class GradeTooLowException: public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+
+	private:
+		const std::string	_name;
+		const int			_signGrade;
+		const int			_exeGrade;
+		bool				_isSigned;
+};
+
+std::ostream &operator<<(std::ostream &os, Form &form);
+
+#endif
